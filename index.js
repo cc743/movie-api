3ce49da3cd02e8d4899c6ffebcf6467fac7c825e
cc743 <67940367+cc743@users.noreply.github.com>
2020-10-20 22:04:53 -0400

    creating express get routes for "/movies" and "/"

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+let topMovies = [
+  {
+    title: "Once Upon a Time in Hollywood",
+    director: "Quentin Tarantino"
+  },
+  {
+    title: "Parasite",
+    director: "Bong Joon-Ho"
+  },
+  {
+    title: "The Mask",
+    director: "Charles Russell"
+  },
+  {
+    title: "A Fistful of Dollars",
+    director: "Sergio Leone"
+  },
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan" //start with 5 movies for now
+  }
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my movie club");
+});
+
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+app.use(express.static("public"));
+
+//listen for requests
+app.listen(8080, () =>
+  console.log("Your app (movie app?) is listening on port 8080.")
+);
