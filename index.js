3c1ed5965ec5c2294c70b38dabc657289681cf39
cc743 <67940367+cc743@users.noreply.github.com>
2020-10-23 00:29:27 -0400

    Further development of documentation table & creation of some (not all) express code to route URL endpoints in index.js

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 const express = require("express");
 const morgan = require("morgan"); //locally importing Morgan
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
 const app = express();
 
 let topMovies = [
   {
     title: "Once Upon a Time in Hollywood",
+    genre: "comedy-drama",
     director: "Quentin Tarantino"
   },
   {
     title: "Parasite",
+    genre: "thriller",
     director: "Bong Joon-Ho"
   },
   {
     title: "The Mask",
+    genre: "comedy",
     director: "Charles Russell"
   },
   {
     title: "A Fistful of Dollars",
+    genre: "western",
     director: "Sergio Leone"
   },
   {
     title: "The Dark Knight",
-    director: "Christopher Nolan" //start with 5 movies for now
+    genre: "superhero",
+    director: "Christopher Nolan"
+  },
+  {
+    title: "It",
+    genre: "horror",
+    director: "Andy Muschietti" //6 movies for now
+  },
+  {
+    title: "Taxi Driver",
+    genre: "drama",
+    director: "Martin Scorsese"
   }
 ];
 
 app.use(morgan("common")); //invoking Morgan
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to my movie club");
@@ -33,6 +52,32 @@ app.get("/", (req, res) => {
 
 app.get("/movies", (req, res) => {
   res.json(topMovies);
+});
+
+//Gets data for a single student by film's tilte
+app.get("/movies/:title", (req, res) => {
+  res.json(
+    topMovies.find(movie => {
+      return movie.title === req.params.title;
+    })
+  );
+});
+
+//Allow new users to register
+app.post("/users", (req, res) => {
+  res.send("Successful POST request for registering a new user");
+});
+
+//Allow an existing user to de-register
+app.delete("/users/:id", (req, res) => {
+  res.send(
+    "Successful DELETE request for deleting an existing user from list of users"
+  );
+});
+
+//Allow users to update their registration information
+app.put("/users/:id", (req, res) => {
+  res.send("Successful PUT request for updating a user's information");
 });
 
 //using this piece of code instead of app.use(express.static('public')) due to technical difficulties
